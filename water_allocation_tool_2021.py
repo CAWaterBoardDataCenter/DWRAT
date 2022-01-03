a07c616d8124e467216bb7055cd3e97240638b0c
D Pedroja <daron.pedroja@waterboards.ca.gov>
2022-01-03 13:29:29 -0800

    output automation

--- water_allocation_tool_2021.py
+++ water_allocation_tool_2021.py
@@ -13,6 +13,9 @@ import pandas as pd
 import datetime
 from main_date_range import date_string
 
+def five_sig_figs(x):
+    output =  f"{x:.5f}"
+    return output
 
 # FIRST SELECT YOUR TIME SERIES DATE FORMAT
 
@@ -27,46 +30,56 @@ data_range = pd.read_csv("input/data_range.csv")
 
 #### for yyyy-mm-dd run this line
 # data_range["Dates"] = data_range["yyyy-mm-dd"]
-
 # #### for yyyy-m-d run this line
 # data_range["Dates"] = data_range["yyyy-m-d"]
-
 # #### for m/d/yyyy run this line
 # data_range["Dates"] = data_range["m/d/yyyy"]
-
 # # #### for monthly yyyy-mm run this line
 data_range["Dates"] = data_range["yyyy-mm"]
 data_range["Dates"].to_csv("output/data_range.csv", header = True)
-
 # check your data range here:
 # data_range["Dates"].unique()
 
 # NOW YOU ARE READY
-
 # READ IN RAW DATA
 # flow
 start = datetime.datetime.now().time()
 flow_table_df = pd.read_csv('input/flows.csv', index_col = "BASIN")
+flow_table_df = flow_table_df[data_range["Dates"].unique()]
 flow_table_df = flow_table_df.sort_index(axis = "index")
-# flow matrix for use later
-flow_matrix = np.array(flow_table_df[data_range["Dates"].unique()])
+# flow matrix
+flow_matrix = np.array(flow_table_df)
+# list of basins
+basins = list(flow_table_df.index)
 
 # riparian demand
 rip_demand_df = pd.read_csv('input/riparian_demand.csv')
 rip_demand_df.set_index("USER", inplace = True)
 rip_demand_df.sort_index(axis = "index", inplace = True)
+rip_demand_df = rip_demand_df[data_range["Dates"].unique()]
+# user basins
+rip_user_data = pd.read_csv('input/riparian_demand.csv', usecols = ["USER","BASIN", "PRIORITY"])
+rip_user_data.set_index("USER", inplace = True)
+rip_user_data.sort_index(axis = "index", inplace = True)
+# list of riparian users
 rip_users = rip_demand_df.index.values
+# riparian demand matrix
+rip_demand_matrix = np.array(rip_demand_df)
 
 # appropriative demand
 app_demand_df = pd.read_csv('input/appropriative_demand.csv')
 app_demand_df.set_index("USER", inplace = True)
 app_demand_df.sort_index(axis = "index", inplace = True)
+# list of appropriative users
 app_users = app_demand_df.index.values
-
 # number of appropriative users 
 app_users_count = np.size(app_users)
+# appropriative demand values only
+app_demand_table = app_demand_df[data_range["Dates"].unique()]
+# appropriative demand matrix
+app_demand_matrix = np.array(app_demand_df[data_range["Dates"].unique()])
     
-# basically just user BASIN in matrix form
+# USER BASIN location in matrix form
 riparian_basin_user_matrix_df = pd.read_csv("input/riparian_user_matrix.csv", index_col="BASIN")
 riparian_basin_user_matrix_df.sort_index(axis = "index", inplace = True)
 riparian_basin_user_matrix_df.sort_index(axis = "columns", inplace = True)
@@ -96,32 +109,34 @@ downstream_connectivity_df.sort_index(axis = "columns", inplace = True)
 downstream_connectivity_matrix = downstream_connectivity_df.to_numpy()
 upstream_connectivity_matrix = downstream_connectivity_matrix.T
 
-# Cumulative (available) flow matrix for use later 
-cumulative_flow_matrix = np.matmul(upstream_connectivity_matrix, flow_table_df[data_range["Dates"].unique()])
-# List of basins
-basins = list(downstream_connectivity_df.index)
-# riparian demand matrix for use later
-rip_demand_matrix = np.array(rip_demand_df[data_range["Dates"].unique()])
-# basin riparian demand matrix for use later
+# # Cumulative (available) flow matrix
+# cumulative_flow_matrix = np.matmul(upstream_connectivity_matrix, flow_table_df)
+
+# basin riparian demand matrix
 rip_basin_demand_matrix = np.matmul(riparian_basin_user_matrix, rip_demand_matrix)
-rip_basin_demand_df = pd.DataFrame(np.matmul(riparian_basin_user_matrix, rip_demand_matrix), index = basins, columns = [data_range["Dates"].unique()]  )
+rip_basin_demand_df = pd.DataFrame(rip_basin_demand_matrix, index = basins, columns = data_range["Dates"].unique())
+# rip_basin_demand_df = pd.DataFrame(np.matmul(riparian_basin_user_matrix, rip_demand_matrix), index = basins, columns = [data_range["Dates"].unique()])
 
-
+# basin appropriative demand matrix
+app_basin_demand_matrix = np.matmul(appropriative_basin_user_matrix, app_demand_matrix)
+app_basin_demand_df = pd.DataFrame(app_basin_demand_matrix, index = basins, columns = data_range["Dates"].unique())
 
 
 ########################### CREATE EMPTY OUTPUT DATAFRAMES #################################
 output_cols = data_range["Dates"].unique().tolist()
 # Riparian output files
-rip_basin_proportions_output = pd.DataFrame(columns=[output_cols], index=basins)
+rip_basin_proportions_output = pd.DataFrame(columns=output_cols, index=basins)
 # Appropriative output files
-app_user_allocations_output = pd.DataFrame(columns=[output_cols], index= app_users)
+app_user_allocations_output = pd.DataFrame(columns=output_cols, index= app_users)
+# appropriative basin allocations
+app_basin_allocations_output = pd.DataFrame(columns=output_cols, index=basins)
+
 ###########################################################################################
 
 
 ################################    START OF RIPARIAN LP     ##############################
 ###########################################################################################
 ###########################################################################################
-
 
 for c, day in enumerate(data_range["Dates"].unique()):
     # print(c, day)
@@ -241,7 +256,7 @@ basin_proportion_matrix = np.array(rip_basin_proportions_output)
 
 # calculate riparian user allocations as 
 # riparian user allocations (basin proportion * user demand)
-rip_user_allocations_output = pd.DataFrame(((np.matmul(riparian_basin_user_matrix.transpose(), basin_proportion_matrix))*rip_demand_matrix),columns= [output_cols] , index=rip_users)
+rip_user_allocations_output = pd.DataFrame(((np.matmul(riparian_basin_user_matrix.transpose(), basin_proportion_matrix))*rip_demand_matrix),columns = output_cols , index=rip_users)
 rip_user_allocations_output.index.name = "USER"  
 # convert allocations to an array
 rip_user_allocations_matrix = np.array(rip_user_allocations_output)
@@ -249,7 +264,7 @@ rip_user_allocations_matrix = np.array(rip_user_allocations_output)
 rip_upstream_allocations_matrix = np.matmul(riparian_user_connectivity_matrix, rip_user_allocations_matrix)
 # aggregate basin riparian allocations
 rip_basin_allocations = np.matmul(riparian_basin_user_matrix, rip_user_allocations_matrix)
-rip_basin_allocations_output = pd.DataFrame(rip_basin_allocations, columns=[output_cols], index=basins)
+rip_basin_allocations_output = pd.DataFrame(rip_basin_allocations, columns= output_cols, index=basins)
 rip_basin_allocations_output.index.name = "BASIN"
 rip_basin_allocations_matrix = np.array(rip_basin_allocations_output)
 
@@ -262,7 +277,7 @@ rip_basin_allocations_matrix # line 244
 
 
 # Matrices calculated from inputs 
-cumulative_flow_matrix # line
+# cumulative_flow_matrix # line
 flow_matrix # line
 rip_basin_demand_matrix # line
 
@@ -297,8 +312,7 @@ rip_upstream_allocations_matrix_new = np.matmul(riparian_user_connectivity_matri
 available_flow_df = pd.DataFrame((cumulative_flow_matrix_new - rip_upstream_allocations_matrix_new), index = basins, columns = output_cols)
 available_flow_df.index.name = "BASIN"
 
-available_flow_df.to_csv("output/available_appropriative_flow.csv")
-#%%
+
 
 #################################       APPROPRIATIVE_LP   ##############################
 #########################################################################################
@@ -360,6 +374,7 @@ for c, day in enumerate(data_range["Dates"].unique()):
         user_allocations.append(user_allocation[user].value())
 
     app_basin_allocations = np.matmul(appropriative_basin_user_matrix, user_allocations)
+    
     print("Basin Appropriative Allocations:") 
     print(app_basin_allocations)
     
@@ -368,78 +383,120 @@ for c, day in enumerate(data_range["Dates"].unique()):
         app_user_allocations_output.loc[i, [day]] = user_allocation[i].varValue
     print(c+1, "of", len(data_range["Dates"].unique()), "complete. Processing day:", day)
     
+    # populate output table        
+    for k, basin in enumerate(basins):
+        app_basin_allocations_output.loc[basin, [day]] = app_basin_allocations[k]
+        
+   
 finish = datetime.datetime.now().time()
 print("Hi. I'm done. Time at completion was:", finish, ". Starting time was:", start)
-'''
-#%%
+
+
 ################################    END OF APPROPRIATIVE LP  ########################
 #####################################################################################
 #####################################################################################
 
-
+#%%
 #################################  WRITING OUTPUT TO .CSV FILES #########################
 #########################################################################################
 #########################################################################################
 #########################################################################################
 
-# basin output
-basin_output_df = available_flow_df[['cumulative_flow', 'basin_flow']]
-basin_output_df["riparian_proportions"] = rip_basin_proportions_output
-basin_output_df["rip_basin_allocations"] = available_flow_df["basin_allocations"] 
-basin_output_df["rip_basin_demand"] = available_flow_df["riparian_demand"]
-basin_output_df["rip_basin_shortage"] = basin_output_df["rip_basin_demand"] - basin_output_df["rip_basin_allocations"] 
-basin_output_df["rip_basin_shortage_%"] = basin_output_df["rip_basin_shortage"] / basin_output_df["rip_basin_demand"]
-basin_output_df["appropriative_output"] = "*"
-basin_output_df["appropriative_available_flow"] = available_flow_df["available_app_flow"]
-basin_output_df["app_basin_allocations"] = app_basin_allocations
-app_demand_matrix = app_demand_df[dates].to_numpy()
-basin_output_df["app_basin_demand"] = np.matmul(appropriative_basin_user_matrix , app_demand_matrix)
-basin_output_df["app_basin_shortage"] = basin_output_df["app_basin_demand"] - basin_output_df["app_basin_allocations"] 
-basin_output_df["app_basin_shortage_%"] = basin_output_df["app_basin_shortage"] / basin_output_df["app_basin_demand"]
-basin_output_df = basin_output_df.add_suffix("_" + day)
-basin_output_df.to_csv("output/basin_output.csv")
+out_file_name = "_" + data_range["Dates"].unique()[1] + "_" + data_range["Dates"].unique()[-1]
 
-# # user output
-# ## Appropriative
+# riparian basin output
+flows_table = flow_table_df.add_suffix("_FLOW")
+demand_table = rip_basin_demand_df.add_suffix("_DEMAND")
+basin_output_df = pd.merge(flows_table, demand_table,  left_index=True, right_index=True)
+proportions_table = rip_basin_proportions_output.add_suffix("_PROPORTIONS")
+basin_output_df = pd.merge(basin_output_df, proportions_table,  left_index=True, right_index=True)
+allocations = rip_basin_allocations_output.add_suffix("_ALLOCATIONS")
+basin_output_df = pd.merge(basin_output_df, allocations,  left_index=True, right_index=True)
+basin_output_df = basin_output_df.sort_index(axis=1)
 
-def five_sig_figs(x):
-    output =  f"{x:.5f}"
-    return output
+basin_output_df.to_csv("output/basin_riparian_output" + out_file_name + ".csv")
 
-user_output_df = app_demand_df[["BASIN", "PRIORITY"]]
-user_output_df["app_allocations"] = app_user_allocations_output[dates]
-user_output_df["app_demand"] = app_demand_df[dates]
-user_output_df["app_shortage"] = (user_output_df["app_demand"] - user_output_df["app_allocations"])
-user_output_df["app_shortage_%"] = (user_output_df["app_shortage"] / user_output_df["app_demand"])*100
-# change values less than 0.00099 to 0 and round to 5 significant figures
-user_output_df["app_shortage"][user_output_df["app_shortage"] < 0.00099] = 0
-user_output_df["app_shortage_%"][user_output_df["app_shortage_%"] < 0.00099] = 0
-user_output_df["app_shortage"] = user_output_df["app_shortage"].apply(five_sig_figs)
-user_output_df["app_shortage_%"] = user_output_df["app_shortage_%"].apply(five_sig_figs)
-user_output_df = user_output_df.add_suffix("_" + day)
-user_output_df.to_csv("output/appropriative_user_output.csv")
+# appropriative basin output
+flows_table_app = available_flow_df.add_suffix("_AVAILABLE_FLOW")
+demand_table_app = app_basin_demand_df.add_suffix("_DEMAND")
+basin_output_df_app = pd.merge(flows_table_app, demand_table_app,  left_index=True, right_index=True)
+allocations_app = app_basin_allocations_output.add_suffix("_ALLOCATIONS")
+basin_output_df_app = pd.merge(basin_output_df_app, allocations_app,  left_index=True, right_index=True)
+basin_output_df_app = basin_output_df_app.sort_index(axis=1)
 
-## Riparian
-del (rip_user_allocations_output)
-rip_demand_matrix = np.array(rip_demand_df[dates])           
-basin_proportion_matrix = np.array(rip_basin_proportions_output[dates])
-rip_user_allocations_output = pd.DataFrame(((np.matmul(riparian_basin_user_matrix.transpose(), basin_proportion_matrix))*rip_demand_matrix), index=rip_demand_df.index)
-rip_user_allocations_output.columns = dates.values
-rip_user_allocations_output.index
+basin_output_df_app.to_csv("output/basin_appropriative_output" + out_file_name + ".csv")
 
-rip_user_output_df = rip_demand_df[["BASIN", "PRIORITY"]]
-rip_user_output_df["rip_allocations"] = rip_user_allocations_output[dates]
-rip_user_output_df["rip_demand"] = rip_demand_df[dates]
-rip_user_output_df["rip_shortage"] = rip_user_output_df["rip_demand"] - rip_user_output_df["rip_allocations"]
-rip_user_output_df["rip_shortage_%"] = rip_user_output_df["rip_shortage"] / rip_user_output_df["rip_demand"]
-# change values less than 0.00099 to 0 and round to 5 significant figures
-rip_user_output_df["rip_shortage"][rip_user_output_df["rip_shortage"] < 0.00099] = 0
-rip_user_output_df["rip_shortage_%"][rip_user_output_df["rip_shortage_%"] < 0.00099] = 0
-rip_user_output_df["rip_shortage"] = rip_user_output_df["rip_shortage"].apply(five_sig_figs)
-rip_user_output_df["rip_shortage_%"] = rip_user_output_df["rip_shortage_%"].apply(five_sig_figs)
-rip_user_output_df = rip_user_output_df.add_suffix("_" + day)
-rip_user_output_df.to_csv("output/riparian_user_output.csv")
+#%%
+# appropriative user output
+# appropriative user shortage
+app_user_shortage_output = pd.DataFrame(columns=output_cols, index= app_users)
+for day in output_cols:
+    app_user_shortage_output[day] = ((app_demand_table[day] - app_user_allocations_output[day])/app_demand_table[day])*100
+    app_user_shortage_output[day][app_user_shortage_output[day] < 1] = 0
+# appropriative user curtailment
+app_user_curtailments_output = pd.DataFrame(columns=output_cols, index= app_users)
+for day in output_cols:
+    app_user_curtailments_output[day][app_user_shortage_output[day] == 100] = 1 
+    app_user_curtailments_output[day][app_user_shortage_output[day] < 100] = 0 
+        
+app_user_shortage_output = app_user_shortage_output.add_suffix("_SHORTAGE_%")
+app_user_curtailments_output = app_user_curtailments_output.add_suffix("_CURTAILMENT")
+app_user_data = app_demand_df[["BASIN", "PRIORITY"]]
+app_user_allocations_output = app_user_allocations_output.add_suffix("_ALLOCATIONS")
+user_output_df_app = pd.merge(app_user_data, app_user_allocations_output,  left_index=True, right_index=True)
+demand_table_app = app_demand_table.add_suffix("_DEMAND")
+user_output_df_app = pd.merge(user_output_df_app, demand_table_app,  left_index=True, right_index=True)
+user_output_df_app = pd.merge(user_output_df_app, app_user_shortage_output,  left_index=True, right_index=True)
+user_output_df_app = pd.merge(user_output_df_app, app_user_curtailments_output,  left_index=True, right_index=True)
+user_output_df_app = user_output_df_app.sort_index(axis=1)
 
+user_output_df_app.to_csv("output/user_appropriative_output" + out_file_name + ".csv")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# riparian user output
+# riparian user shortage
+rip_user_shortage_output = pd.DataFrame(columns=output_cols, index= rip_users)
+for day in output_cols:
+    rip_user_shortage_output[day] = ((rip_demand_df[day] - rip_user_allocations_output[day])/rip_demand_df[day])*100
+    rip_user_shortage_output[day][rip_user_shortage_output[day] < 1] = 0
+# riparian user curtailment
+rip_user_curtailments_output = pd.DataFrame(columns=output_cols, index= rip_users)
+for day in output_cols:
+    rip_user_curtailments_output[day][rip_user_shortage_output[day] > 0] = 1 
+    rip_user_curtailments_output[day][rip_user_shortage_output[day] == 0] = 0 
+        
+rip_user_shortage_output = rip_user_shortage_output.add_suffix("_SHORTAGE_%")
+rip_user_curtailments_output = rip_user_curtailments_output.add_suffix("_CURTAILMENT")
+rip_user_data = rip_user_data[["BASIN", "PRIORITY"]]
+
+rip_user_allocations_output = rip_user_allocations_output.add_suffix("_ALLOCATIONS")
+user_output_df_rip = pd.merge(rip_user_data, rip_user_allocations_output,  left_index=True, right_index=True)
+rip_demand_df = rip_demand_df.add_suffix("_DEMAND")
+user_output_df_rip = pd.merge(user_output_df_rip, rip_demand_df,  left_index=True, right_index=True)
+user_output_df_rip = pd.merge(user_output_df_rip, rip_user_shortage_output,  left_index=True, right_index=True)
+user_output_df_rip = pd.merge(user_output_df_rip, rip_user_curtailments_output,  left_index=True, right_index=True)
+user_output_df_rip = user_output_df_rip.sort_index(axis=1)
+
+user_output_df_rip.to_csv("output/user_riparian_output" + out_file_name + ".csv")
+
+#%%
+'''
 #################################  WRITING OUTPUT FOR PORTAL TOOL########################
 #########################################################################################
 #########################################################################################
